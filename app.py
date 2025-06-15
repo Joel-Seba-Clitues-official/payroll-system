e6316aafba80895e8c5ff726c14633f526c25ac5
JOEL SEBA CLITUES J <166889563+Joel-Seba-Clitues-official@users.noreply.github.com>
2025-06-15 18:41:45 +0530

    Update app.py
    port change

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def add_employee():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)  # Change port as needed
+    app.run(debug=True, host='0.0.0.0', port=5000) # Change port as needed
